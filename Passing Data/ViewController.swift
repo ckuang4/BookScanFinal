14708e39882b56e5b722e8b5d82a3c61bb781a63
ckuang4 <ckuang@college.harvard.edu>
2016-12-07 12:23:37 -0500

    Initial Commit

---
+++ Passing Data/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Passing Data
+//
+//  Created by Chris Kuang on 12/7/16.
+//  Copyright © 2016 Chris Kuang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
